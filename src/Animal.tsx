6e85a4a0c8c6b48dd80b1c11d8436470ab75a039
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2024-12-02 20:44:52 -0300

    Adicionando a lógica do peso do animal

--- src/Animal.tsx
+++ src/Animal.tsx
@@ -11,7 +11,9 @@ function Animal({ icone, nome, peso, emExtincao }: AnimalProps) {
   return <div className="animal">
     <div className="icone">{icone}</div>
     <div className="nome">{nome}</div>
-    <div className="peso">{peso.toLocaleString()} kg</div>
+    {
+        peso > 0 ? <div className="peso">{peso.toLocaleString()} kg</div> : <div className="peso">Peso desconhecido</div>
+    }
     {emExtincao && <div className="extincao">Em extinção!</div>}
   </div>;
 }
--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="app">
       <Animal icone="🦁" nome="Leão" peso={190.0} emExtincao={true} />
       <Animal icone="🦩" nome="Flamingo" peso={12.0} emExtincao={true} />
-      <Animal icone="🦒" nome="Girafa" peso={1200.0} emExtincao={true} />
+      <Animal icone="🦒" nome="Girafa" peso={0.0} emExtincao={true} />
       <Animal icone="🦜" nome="Papagaio" peso={0.4} emExtincao={false} />
     </div>
   );
