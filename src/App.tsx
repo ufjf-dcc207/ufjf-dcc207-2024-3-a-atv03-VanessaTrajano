31cdf57af93a64f58ae635a6ed97e9b13a0c22d9
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2024-12-02 20:31:57 -0300

    Adiciona outras chamadas de Animal

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="app">
       <Animal icone="🦁" nome="Leão" peso={190.0} emExtincao={true} />
+      <Animal icone="🦩" nome="Flamingo" peso={12.0} emExtincao={true} />
+      <Animal icone="🦒" nome="Girafa" peso={1200.0} emExtincao={true} />
+      <Animal icone="🦜" nome="Papagaio" peso={0.4} emExtincao={false} />
     </div>
   );
 }
