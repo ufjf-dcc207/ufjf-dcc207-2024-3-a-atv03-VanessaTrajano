12b05d9168f0cdb5e770c1c1fe09288f76a225eb
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2024-12-02 20:21:19 -0300

    Recebendo props no componente Animal

--- src/Animal.tsx
+++ src/Animal.tsx
@@ -1,7 +1,19 @@
 import "./Animal.css";
 
-function Animal() {
-  return <div className="animal">Animal</div>;
+interface AnimalProps {
+  icone: string;
+  nome: string;
+  peso: number;
+  emExtincao: boolean;
+}
+
+function Animal({ icone, nome, peso, emExtincao }: AnimalProps) {
+  return <div className="animal">
+    <div>{icone}</div>
+    <div>{nome}</div>
+    <div>{peso.toLocaleString()} kg</div>
+    <div>Em extinção: {emExtincao}</div>
+  </div>;
 }
 
 export default Animal;
