6abcd465f495aacd1ca36ef0c7c23c293c4f1f48
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2024-12-05 21:16:42 -0300

    Formata semântica

--- src/Animal.tsx
+++ src/Animal.tsx
@@ -8,14 +8,18 @@ interface AnimalProps {
 }
 
 function Animal({ icone, nome, peso, emExtincao }: AnimalProps) {
-  return <div className="animal">
-    <div className="icone">{icone}</div>
-    <div className="nome">{nome}</div>
-    {
-        peso > 0 ? <div className="peso">{peso.toLocaleString()} kg</div> : <div className="peso">Peso desconhecido</div>
-    }
-    {emExtincao && <div className="extincao">Em extinção!</div>}
-  </div>;
+  return (
+    <div className="animal">
+      <div className="icone">{icone}</div>
+      <div className="nome">{nome}</div>
+      {peso > 0 ? (
+        <div className="peso">{peso.toLocaleString()} kg</div>
+      ) : (
+        <div className="peso">Peso desconhecido</div>
+      )}
+      {emExtincao && <div className="extincao">Em extinção!</div>}
+    </div>
+  );
 }
 
 export default Animal;
