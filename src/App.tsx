3c8b7f9a102f04828ae8d8725da891c7fd048d80
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2024-12-05 21:20:11 -0300

    Cria o componente Exibicao

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Animal from "./Animal";
+import Exibicao from "./Exibicao";
 
 function App() {
   return (
     <div className="app">
+      <Exibicao/>
       <Animal icone="🦁" nome="Leão" peso={190.0} emExtincao={true} />
       <Animal icone="🦩" nome="Flamingo" peso={12.0} emExtincao={true} />
       <Animal icone="🦒" nome="Girafa" peso={0.0} emExtincao={true} />
---
+++ src/Exibicao.tsx
@@ -0,0 +1,11 @@
+import "./Exibicao.css";
+
+function Exibicao() {
+  return (
+    <div className="exibicao">
+      Exibicao
+    </div>
+  );
+}
+
+export default Exibicao;
