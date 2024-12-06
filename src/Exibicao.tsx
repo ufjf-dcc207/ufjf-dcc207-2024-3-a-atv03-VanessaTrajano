7eb88b5d93cb4251369f2cbe6878835508ee9e1e
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2024-12-05 21:45:26 -0300

    Melhora e estiliza o componente Extincao

--- src/Exibicao.tsx
+++ src/Exibicao.tsx
@@ -1,9 +1,17 @@
 import "./Exibicao.css";
 
-function Exibicao() {
+interface ExibicaoProps {
+    abertura: Date;
+    fechamento: Date;
+    cercado: string;
+}
+
+function Exibicao({abertura:inicio, fechamento:fim, cercado:local}:ExibicaoProps) {
   return (
     <div className="exibicao">
-      Exibicao
+      <div className="inicio">{inicio.toLocaleString()}</div>
+      <div className="fim">{fim.toLocaleString()}</div>
+      <div className="local">{local}</div>
     </div>
   );
 }
