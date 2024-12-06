bf3fdebadeb63201c2371c402f0affea8af824cf
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2024-12-05 22:11:34 -0300

    Adiciona filhos na Exibicao

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,18 @@ function App() {
         abertura={new Date("2024-12-06T08:00:00.000-03:00")}
         fechamento={new Date("2024-12-06T16:00-03:00")}
         cercado="A1"
-      />
-      <Animal icone="🦁" nome="Leão" peso={190.0} emExtincao={true} />
-      <Animal icone="🦩" nome="Flamingo" peso={12.0} emExtincao={true} />
+      >
+        <Animal icone="🦁" nome="Leão" peso={190.0} emExtincao={true} />
+        <Animal icone="🦩" nome="Flamingo" peso={12.0} emExtincao={true} />
+      </Exibicao>
       <Exibicao
         abertura={new Date("2024-12-06T08:00:00.000-03:00")}
         fechamento={new Date("2024-12-06T16:00-03:00")}
         cercado="A2"
-      />
-      <Animal icone="🦒" nome="Girafa" peso={0.0} emExtincao={true} />
-      <Animal icone="🦜" nome="Papagaio" peso={0.4} emExtincao={false} />
+      >
+        <Animal icone="🦒" nome="Girafa" peso={0.0} emExtincao={true} />
+        <Animal icone="🦜" nome="Papagaio" peso={0.4} emExtincao={false} />
+      </Exibicao>
     </div>
   );
 }
--- src/Exibicao.tsx
+++ src/Exibicao.tsx
@@ -1,17 +1,25 @@
+import { ReactNode } from "react";
 import "./Exibicao.css";
 
 interface ExibicaoProps {
-    abertura: Date;
-    fechamento: Date;
-    cercado: string;
+  abertura: Date;
+  fechamento: Date;
+  cercado: string;
+  children?: ReactNode;
 }
 
-function Exibicao({abertura:inicio, fechamento:fim, cercado:local}:ExibicaoProps) {
+function Exibicao({
+  abertura: inicio,
+  fechamento: fim,
+  cercado: local,
+  children: animais,
+}: ExibicaoProps) {
   return (
     <div className="exibicao">
       <div className="inicio">Abertura: {inicio.toLocaleString()}</div>
       <div className="fim">Fechamento: {fim.toLocaleString()}</div>
       <div className="local">Cercado: {local}</div>
+      <div className="animais">{animais}</div>
     </div>
   );
 }
