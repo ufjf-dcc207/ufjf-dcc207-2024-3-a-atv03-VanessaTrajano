c97279c32bd778004018c06326d9c885815df9a5
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2024-12-09 19:25:39 -0300

    Extrai todos os componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import "./App.css";
 import Animal from "./Animal";
 import Exibicao from "./Exibicao";
+import { ReactNode } from "react";
 
 function App() {
+  const exA1: ReactNode[] = [
+    <Animal icone="🦁" nome="Leão" peso={190.0} emExtincao={true} />,
+    <Animal icone="🦩" nome="Flamingo" peso={12.0} emExtincao={true} />,
+  ];
+  const exA2: ReactNode[] = [
+    <Animal icone="🦒" nome="Girafa" peso={0.0} emExtincao={true} />,
+    <Animal icone="🦜" nome="Papagaio" peso={0.4} emExtincao={false} />,
+  ];
+
   return (
     <div className="app">
       <Exibicao
@@ -10,16 +20,14 @@ function App() {
         fechamento={new Date("2024-12-06T16:00-03:00")}
         cercado="A1"
       >
-        <Animal icone="🦁" nome="Leão" peso={190.0} emExtincao={true} />
-        <Animal icone="🦩" nome="Flamingo" peso={12.0} emExtincao={true} />
+        {exA1}
       </Exibicao>
       <Exibicao
         abertura={new Date("2024-12-06T08:00:00.000-03:00")}
         fechamento={new Date("2024-12-06T16:00-03:00")}
         cercado="A2"
       >
-        <Animal icone="🦒" nome="Girafa" peso={0.0} emExtincao={true} />
-        <Animal icone="🦜" nome="Papagaio" peso={0.4} emExtincao={false} />
+        {exA2}
       </Exibicao>
     </div>
   );
